c8c913d374a7767e0e7774aa9d754e3c433fd565
Notarin Steele <424c414e4b@gmail.com>
2025-11-26 23:59:07 -0500

    Shrunk some logic

--- flake.nix
+++ flake.nix
@@ -2,51 +2,43 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      systems = [ "x86_64-linux" ];
-      buildEachSystem = output: builtins.map (system: output system) systems;
-      buildAllSystems =
-        output:
-        (builtins.foldl' (acc: elem: nixpkgs.lib.recursiveUpdate acc elem) { } (buildEachSystem output));
-    in
-    (buildAllSystems (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux"];
+    buildAllSystems = output: builtins.foldl' nixpkgs.lib.recursiveUpdate {} (builtins.map output systems);
+  in (buildAllSystems (
+    system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages.${system} = {
+        default = self.packages.${system}.votv-wine-wayland;
+        votv-wine-wayland = pkgs.callPackage ./package.nix {wine = pkgs.wineWow64Packages.waylandFull;};
+        votv-wine64 = pkgs.callPackage ./package.nix {wine = pkgs.wine64;};
+        votv-wine = pkgs.callPackage ./package.nix {wine = pkgs.wine;};
+      };
+      apps.${system} = {
+        default = self.apps.${system}.votv-wine-wayland;
+        votv-wine-wayland = {
+          type = "app";
+          program = "${pkgs.lib.getExe self.packages.${system}.votv-wine-wayland}";
+          meta.description = "An early-access indie horror simulation video game";
         };
-      in
-      {
-        packages.${system} = {
-          default = self.packages.${system}.votv-wine-wayland;
-          votv-wine-wayland = pkgs.callPackage ./package.nix { wine = pkgs.wineWow64Packages.waylandFull; };
-          votv-wine64 = pkgs.callPackage ./package.nix { wine = pkgs.wine64; };
-          votv-wine = pkgs.callPackage ./package.nix { wine = pkgs.wine; };
+        votv-wine64 = {
+          type = "app";
+          program = "${pkgs.lib.getExe self.packages.${system}.votv-wine64}";
+          meta.description = "An early-access indie horror simulation video game";
         };
-        apps.${system} = {
-          default = self.apps.${system}.votv-wine-wayland;
-          votv-wine-wayland = {
-            type = "app";
-            program = "${pkgs.lib.getExe self.packages.${system}.votv-wine-wayland}";
-            meta.description = "An early-access indie horror simulation video game";
-          };
-          votv-wine64 = {
-            type = "app";
-            program = "${pkgs.lib.getExe self.packages.${system}.votv-wine64}";
-            meta.description = "An early-access indie horror simulation video game";
-          };
-          votv-wine = {
-            type = "app";
-            program = "${pkgs.lib.getExe self.packages.${system}.votv-wine}";
-            meta.description = "An early-access indie horror simulation video game";
-          };
+        votv-wine = {
+          type = "app";
+          program = "${pkgs.lib.getExe self.packages.${system}.votv-wine}";
+          meta.description = "An early-access indie horror simulation video game";
         };
-        formatter.${system} = pkgs.nixfmt-tree;
-      }
-    ));
+      };
+      formatter.${system} = pkgs.nixfmt-tree;
+    }
+  ));
 }
